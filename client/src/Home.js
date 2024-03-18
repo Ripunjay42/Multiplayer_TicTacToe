1449bc3a1805079e9ce2072d13cfa30bf8ccc11d
Ripunjay choudhury <ripunjaychoudhury42@gmail.com>
2024-03-18 07:03:47 +0530

    Update Home.js

--- client/src/Home.js
+++ client/src/Home.js
@@ -22,7 +22,7 @@ function Home()
                 <h1>TIC-TAC-TOE</h1>
             </div>
             <div className='gifi'>
-                <img  width='350px' height='350px' src={image} alt="gif"/>
+                <img  width='340px' height='340px' src={image} alt="gif"/>
             </div>
             <div className='play1'>
                 <button onClick={handleClick}>Lets Play</button>
